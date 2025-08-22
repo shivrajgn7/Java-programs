6d44936c33788f8d2fa1da70f0313d8bc4d45f67
shivrajgn7 <shivrajgn7@.com>
2025-08-22 10:05:53 +0530

    commiting java program

--- bank-account/src/com/sbi/bankaccount/BankRunner.java
+++
@@ -1,8 +0,0 @@
-package com.sbi.bankaccount;
-
-import com.sbi.bankaccount.account.BankAccount;
-
-public class BankRunner {
-    BankAccount shivrajAccount=new BankAccount();
-    shivrajAccount.transfer(friendAccount,1000.00);
-}
--- filemanager/src/com/microsoft/files/FileManager.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.files;
-
-import java.security.spec.RSAOtherPrimeInfo;
-
-public class FileManager {
-    public static void main(String[] args) {
-        System.out.printf("help to manage the files");
-    }
-}
